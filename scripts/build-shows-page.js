bc35854471f1a1ad461b341d9ecdd66d79b22d20
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-03-23 16:26:56 -0400

    loop idea

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -9,6 +9,54 @@ heading.innerText = 'Shows';
 heading.className = 'shows__title'
 sectionEl.appendChild(heading);
 
+const container = document.createElement('div');
+container.className = 'shows__container'
+sectionEl.appendChild(container);
+
+
+
+const shows = [
+    {
+        Date: 'Mon Sept 06, 2021',
+        Venue: 'Ronald Lane',
+        Location: 'San Francisco, CA',
+    },
+    {
+        Date: 'Tue Sept 21, 2021',
+        Venue: 'Pier 3 East',
+        Location: 'San Francisco, CA',
+    },
+    {
+        Date: 'Fri Oct 15, 2021',
+        Venue: 'View Lounge',
+        Location: 'San Francisco, CA',
+    },
+    {
+        Date: 'Sat Nov 06, 2021',
+        Venue: 'Hyatt Agency',
+        Location: 'San Francisco, CA',
+    },
+    {
+        Date: 'Fri Nov 26, 2021',
+        Venue: 'Moscow Center',
+        Location: 'San Francisco, CA',
+    },
+    {
+        Date: 'Wed Dec 15 2021',
+        Venue: 'Press Clubt',
+        Location: 'San Francisco, CA',
+    },
+
+]
+
+
+for (let i = 0; i < shows.length; i++){
+    let showData = document.createElement('div');
+    showData.innerText = `${shows[i].Date} ${shows[i].Venue} ${shows[i].Location}`;
+    showData.classList.add('shows__data');
+    container.appendChild(showData);
+
+}
 
 
 
@@ -26,39 +74,3 @@ sectionEl.appendChild(heading);
 
 
 
-
-
-
-// const shows = [
-//     {
-//         Date: 'Mon Sept 06, 2021',
-//         Venue: 'Ronald Lane',
-//         Location: 'San Francisco, CA',
-//     },
-//     {
-//         Date: 'Tue Sept 21, 2021',
-//         Venue: 'Pier 3 East',
-//         Location: 'San Francisco, CA',
-//     },
-//     {
-//         Date: 'Fri Oct 15, 2021',
-//         Venue: 'View Lounge',
-//         Location: 'San Francisco, CA',
-//     },
-//     {
-//         Date: 'Sat Nov 06, 2021',
-//         Venue: 'Hyatt Agency',
-//         Location: 'San Francisco, CA',
-//     },
-//     {
-//         Date: 'Fri Nov 26, 2021',
-//         Venue: 'Moscow Center',
-//         Location: 'San Francisco, CA',
-//     },
-//     {
-//         Date: 'Wed Dec 15 2021',
-//         Venue: 'Press Clubt',
-//         Location: 'San Francisco, CA',
-//     },
-
-// ]
