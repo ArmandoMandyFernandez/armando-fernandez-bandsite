21cffd5bf093e075b263bb6fb2ebbc5e932ee35a
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-03-26 10:42:47 -0400

    got comments to show up finally

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -5,3 +5,77 @@ let comments = [
     { name: "Emilie beach", timestamp: "01/09/2021", comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life i could relive, this would be it. What an incredible day." },
     { name: "Miles Acosta", timestamp: "12/20/2020", comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough." }
   ];
+
+  const displayComment = (comment) => {
+    const commentList = document.getElementById('comments__list');
+  
+    const commentContainer = document.createElement('div');
+    commentContainer.classList.add('comments__comment-container');
+  
+    const commentImage = document.createElement('div');
+    commentImage.classList.add('comments__comment-image');
+  
+    const commentHeader = document.createElement('div');
+    commentHeader.classList.add('comments__comment-header');
+  
+    const commentName = document.createElement('h4');
+    commentName.classList.add('comments__comment-name');
+    commentName.innerText = comment.name;
+  
+    const commentDate = document.createElement('span');
+    commentDate.classList.add('comments__comment-date');
+    commentDate.innerText = comment.timestamp;
+  
+    const commentBody = document.createElement('p');
+    commentBody.classList.add('comments__comment-body');
+    commentBody.innerText = comment.comment;
+  
+    commentHeader.appendChild(commentImage);
+    commentHeader.appendChild(commentName);
+    commentHeader.appendChild(commentDate);
+  
+    commentContainer.appendChild(commentHeader);
+    commentContainer.appendChild(commentBody);
+  
+    commentList.insertBefore(commentContainer, commentList.firstChild);
+  };
+  
+  const renderComments = () => {
+    const commentList = document.getElementById('comments__list');
+    commentList.innerHTML = '';
+  
+    for (let i = comments.length - 1; i >= 0; i--) {
+      displayComment(comments[i]);
+    }
+  };
+  
+  const addComment = (event) => {
+    event.preventDefault();
+  
+    const nameInput = document.querySelector('.comments__name-label-input');
+    const commentInput = document.querySelector('.comments__comment-label-input');
+  
+    const name = nameInput.value;
+    const comment = commentInput.value;
+    const timestamp = new Date().toLocaleDateString();
+  
+    const newComment = {
+      name,
+      timestamp,
+      comment
+    };
+  
+    comments.unshift(newComment);
+  
+    nameInput.value = '';
+    commentInput.value = '';
+  
+    renderComments();
+  };
+  
+  const commentForm = document.querySelector('.comments__form');
+  commentForm.addEventListener('submit', addComment);
+  
+  renderComments();
+  
+  
