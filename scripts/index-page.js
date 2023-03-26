ac7d8c1465cd94acf32359ac657329aeda00e6c9
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-03-26 09:38:09 -0400

    styling the comments section

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,2 +1,7 @@
 console.log('hello Mandy');
 
+let comments = [
+    { name: "Connor Walton", timestamp: "02/17/2021", comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contatins." },
+    { name: "Emilie beach", timestamp: "01/09/2021", comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life i could relive, this would be it. What an incredible day." },
+    { name: "Miles Acosta", timestamp: "12/20/2020", comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough." }
+  ];
