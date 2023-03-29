7e424d4796b332407b4f9937b429df667b245d72
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-03-29 00:17:02 -0400

    completed shows page from bandsite 2 exactly the way it was supposed to be

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,4 +1,4 @@
-console.log('hello');
+console.log('hello mandy');
 
 const shows = [
     {
@@ -41,74 +41,238 @@ function createShows(shows){
     const section = document.createElement('section');
     section.className = ('shows');
 
-    //creating the heading 'Shows'
     const heading = document.createElement('h2');
     heading.innerText = 'Shows';
     heading.className = 'shows__title';
     section.appendChild(heading);
 
-    // creating the div container for all the data
+    const mainContainer = document.createElement('div');
+    mainContainer.className = ('shows__container-main');
+    section.appendChild(mainContainer)
+
     const container = document.createElement('div');
-    container.className = 'shows__container';
+    container.className= 'shows__container-header'
+    mainContainer.appendChild(container);
 
-    //create the for loop to grab all the info
+    const containerLabel1 = document.createElement('p');
+    containerLabel1.className = 'shows__container-header-label'
+    containerLabel1.innerText = 'DATE';
+    container.appendChild(containerLabel1);
+
+    const containerLabel2 = document.createElement('p');
+    containerLabel2.className = 'shows__container-header-label'
+    containerLabel2.innerText = 'VENUE';
+    container.appendChild(containerLabel2);
+
+    const containerLabel3 = document.createElement('p');
+    containerLabel3.className = 'shows__container-header-label'
+    containerLabel3.innerText = 'LOCATION';
+    container.appendChild(containerLabel3);
+
+    const button = document.createElement('button');
+    button.className = 'shows__button-hidden';
+    button.innerText = 'BUY TICKETS';
+    container.appendChild(button);
+
+    const dataContainer = document.createElement('div');
+    dataContainer.className = 'shows__container-data';
+    mainContainer.appendChild(dataContainer);
+
+
     shows.forEach(show => {
-        
-        // div container for the show info
-        const dataContainer = document.createElement('div');
-        dataContainer.className = 'shows__data';
 
-        //shows date which should show up first
+        const showsCard = document.createElement('div');
+        showsCard.className = 'shows__card';
+
         const dateLabel = document.createElement('p');
         dateLabel.className = 'shows__label';
         dateLabel.innerText = 'DATE';
-        dataContainer.appendChild(dateLabel);
+        showsCard.appendChild(dateLabel);
 
         const date = document.createElement('h3');
         date.className = 'shows__date';
         date.innerText = show.date;
-        dataContainer.appendChild(date);
+        showsCard.appendChild(date);
 
-        //venue of the show
         const venueLabel = document.createElement('p');
         venueLabel.className = 'shows__label';
         venueLabel.innerText = 'VENUE';
-        dataContainer.appendChild(venueLabel);
+        showsCard.appendChild(venueLabel);
 
         const venue = document.createElement('p');
         venue.className = 'shows__venue';
         venue.innerText = show.venue;
-        dataContainer.appendChild(venue);
+        showsCard.appendChild(venue);
 
-        //shows location
         const locationLabel = document.createElement('p');
         locationLabel.className = 'shows__label';
         locationLabel.innerText = 'LOCATION';
-        dataContainer.appendChild(locationLabel);
+        showsCard.appendChild(locationLabel);
 
         const location = document.createElement('p');
         location.className = 'shows__location';
         location.innerText = show.location;
-        dataContainer.appendChild(location);
+        showsCard.appendChild(location);
 
-        //shows button for tickets
         const button = document.createElement('button');
         button.className = 'shows__button';
         button.innerText = 'BUY TICKETS';
-        dataContainer.appendChild(button);
+        showsCard.appendChild(button);
 
-        container.appendChild(dataContainer);        
+        dataContainer.appendChild(showsCard);  
+
     });
-
-    section.appendChild(container);
+    mainContainer.appendChild(dataContainer);
     return section;
 }
 
-const showDataContainers = document.querySelectorAll('.shows__data');
+const showsCard = document.querySelectorAll('.shows__card');
 
-showDataContainers.forEach(container => {
-  container.addEventListener('click', () => {
-    showDataContainers.forEach(c => c.classList.remove('selected'));
-    container.classList.add('selected');
-  });
+showsCard.forEach(container => {
+    container.addEventListener('click', ()=>{
+        showsCard.forEach(c => c.classList.remove('selected'));
+        container.classList.add('selected');
+    });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('hello');
+
+// const shows = [
+//     {
+//         date: 'Mon Sept 06 2021',
+//         venue: 'Ronald Lane',
+//         location: 'San Francisco, CA',
+//     },
+//     {
+//         date: 'Tue Sept 21 2021',
+//         venue: 'Pier 3 East',
+//         location: 'San Francisco, CA',
+//     },
+//     {
+//         date: 'Fri Oct 15 2021',
+//         venue: 'View Lounge',
+//         location: 'San Francisco, CA',
+//     },
+//     {
+//         date: 'Sat Nov 06 2021',
+//         venue: 'Hyatt Agency',
+//         location: 'San Francisco, CA',
+//     },
+//     {
+//         date: 'Fri Nov 26 2021',
+//         venue: 'Moscow Center',
+//         location: 'San Francisco, CA',
+//     },
+//     {
+//         date: 'Wed Dec 15 2021',
+//         venue: 'Press Club',
+//         location: 'San Francisco, CA',
+//     },
+// ];
+
+// const showsSection = createShows(shows);
+// document.querySelector('.hero').after(showsSection);
+
+// function createShows(shows){
+//     //creating shows section
+//     const section = document.createElement('section');
+//     section.className = ('shows');
+
+//     //creating the heading 'Shows'
+//     const heading = document.createElement('h2');
+//     heading.innerText = 'Shows';
+//     heading.className = 'shows__title';
+//     section.appendChild(heading);
+
+//     // creating the div container for all the data
+//     const container = document.createElement('div');
+//     container.className = 'shows__container';
+
+//     //create the for loop to grab all the info
+//     shows.forEach(show => {
+        
+//         // div container for the show info
+//         const dataContainer = document.createElement('div');
+//         dataContainer.className = 'shows__data';
+
+//         //shows date which should show up first
+//         const dateLabel = document.createElement('p');
+//         dateLabel.className = 'shows__label';
+//         dateLabel.innerText = 'DATE';
+//         dataContainer.appendChild(dateLabel);
+
+//         const date = document.createElement('h3');
+//         date.className = 'shows__date';
+//         date.innerText = show.date;
+//         dataContainer.appendChild(date);
+
+//         //venue of the show
+//         const venueLabel = document.createElement('p');
+//         venueLabel.className = 'shows__label';
+//         venueLabel.innerText = 'VENUE';
+//         dataContainer.appendChild(venueLabel);
+
+//         const venue = document.createElement('p');
+//         venue.className = 'shows__venue';
+//         venue.innerText = show.venue;
+//         dataContainer.appendChild(venue);
+
+//         //shows location
+//         const locationLabel = document.createElement('p');
+//         locationLabel.className = 'shows__label';
+//         locationLabel.innerText = 'LOCATION';
+//         dataContainer.appendChild(locationLabel);
+
+//         const location = document.createElement('p');
+//         location.className = 'shows__location';
+//         location.innerText = show.location;
+//         dataContainer.appendChild(location);
+
+//         //shows button for tickets
+//         const button = document.createElement('button');
+//         button.className = 'shows__button';
+//         button.innerText = 'BUY TICKETS';
+//         dataContainer.appendChild(button);
+
+//         container.appendChild(dataContainer);        
+//     });
+
+//     section.appendChild(container);
+//     return section;
+// }
+
+// const showDataContainers = document.querySelectorAll('.shows__data');
+
+// showDataContainers.forEach(container => {
+//   container.addEventListener('click', () => {
+//     showDataContainers.forEach(c => c.classList.remove('selected'));
+//     container.classList.add('selected');
+//   });
+// });
+
+// const showCardContainer = document.querySelectorAll ('.shows__card');
+
+// showCardContainer.forEach(container => {
+//     showsContainerData.addEventListener('click',() =>{
+//         showCardContainer.forEach(c => c.classList.remove('selected'));
+//         showsContainerData.classList.add('selected');
+//     });
+// });
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -7,10 +7,14 @@ const APIKey = '844592e2-0f3f-48b8-b6a7-a111bd6d8554'
 axios.get('https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.4/axios.min.js');
 
 let comments = [
-    { name: "Connor Walton", timestamp: "02/17/2021", comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contatins." },
-    { name: "Emilie beach", timestamp: "01/09/2021", comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life i could relive, this would be it. What an incredible day." },
-    { name: "Miles Acosta", timestamp: "12/20/2020", comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough." }
-  ];
+  get()
+]
+
+// let comments = [
+//     { name: "Connor Walton", timestamp: "02/17/2021", comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contatins." },
+//     { name: "Emilie beach", timestamp: "01/09/2021", comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life i could relive, this would be it. What an incredible day." },
+//     { name: "Miles Acosta", timestamp: "12/20/2020", comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough." }
+//   ];
 
   const displayComment = (comment) => {
     const commentList = document.getElementById('comments__list');
